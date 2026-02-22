832bc4acb727188b255c9d769a6290df089f0de8
Muhammad Mudasir Munir Khan <139485101+Muds1r@users.noreply.github.com>
2026-02-23 02:32:30 +0500

    Nav,Hero,LC Done

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    if (products && products.length > 0) {
+      setLatestProducts(products.slice(0, 10));
+    }
+  }, [products]);
+
+  return (
+    <div className="my-10">
+      {/* Title Section */}
+      <div className="text-center py-8 text-3xl font-bold">
+        <Title text1="Latest" text2="COLLECTION" />
+        <p className="w-3/4 m-auto text-xs sm:text-xs md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum autem commodi nesciunt impedit quae! 
+          Vero, quasi corporis sapiente alias molestias molestiae id dignissimos.
+        </p>
+      </div>
+
+      {/* Products Grid */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/hero'
+import LatestCollection from '../components/LatestCollection.Jsx'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection/>
+    </div>
+  )
+}
+
+export default Home
