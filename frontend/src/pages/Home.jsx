64e0d30a1c1ed7446cfc0354c9c6dd97b3ab4833
Muhammad Mudasir Munir Khan <139485101+Muds1r@users.noreply.github.com>
2026-02-24 01:11:13 +0500

    Added BestSeller To Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Hero from '../components/hero'
 import LatestCollection from '../components/LatestCollection.Jsx'
+import BestSeller from '../components/BestSeller'
 
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <LatestCollection/>
+      <LatestCollection />
+      <BestSeller />
     </div>
   )
 }
