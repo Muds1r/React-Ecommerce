addec80cbb689ecbc404d4ebc71325a472c1875f
Muhammad Mudasir Munir Khan <139485101+Muds1r@users.noreply.github.com>
2026-03-08 01:46:27 +0500

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection.Jsx'
+import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import Newsletterbox from '../components/Newsletterbox'
